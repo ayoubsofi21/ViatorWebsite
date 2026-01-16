778a9dc204708aaa0388488eaa08e24933e2df98
ayoubsofi <ayoubsofi03@email.com>
2026-01-16 17:39:21 +0100

    third commit

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
-window.onload = function () {
-  const container = document.querySelector(".image-container");
-  const imageFolder = "images/";
-  const images = ["8f.jpg", "15.jpg", "19.jpg"];
-  images.forEach((image) => {
-    const imgElement = document.createElement("img");
-    imgElement.src = imageFolder + image;
-    imgElement.alt = image;
-    imgElement.classList.add("image");
-    container.appendChild(imgElement);
-  });
-};
+images = [
+  { src: "images/8f.jpg", name: "Paris" },
+  { src: "images/15.jpg", name: "Rome " },
+  { src: "images/19.jpg", name: "Paris" },
+  { src: "images/47.jpg", name: "Paris" },
+  { src: "images/b0.jpg", name: "Paris" },
+  { src: "images/bd.jpg", name: "Paris" },
+  { src: "images/c0.jpg", name: "Paris" },
+  { src: "images/cc.jpg", name: "Paris" },
+];
+
+const container = document.querySelector(".image-container");
+images.forEach((image) => {
+  const divClass = document.createElement("div");
+  const imgElement = document.createElement("img");
+  const PName = document.createElement("p");
+  imgElement.src = image.src;
+  imgElement.alt = image.name;
+  PName.textContent = image.name;
+  divClass.appendChild(imgElement);
+  divClass.appendChild(PName);
+  container.appendChild(divClass);
+});
